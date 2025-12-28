810297cdb66a1dc6c3e6121cd5f87b6a614f82f5
PerumalFairpay <perumal4fairpay@gmail.com>
2025-12-28 20:11:44 +0530

    1

--- store/rootReducer.ts
+++ store/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import blogReducer from "./blog/reducer";
 import authReducer from "./auth/reducer";
+import settingsReducer from "./settings/reducer";
 
 const rootReducer = combineReducers({
       Blog: blogReducer,
       Auth: authReducer,
+      Settings: settingsReducer,
 });
 
 export type AppState = ReturnType<typeof rootReducer>;
--- store/rootSaga.ts
+++ store/rootSaga.ts
@@ -1,10 +1,12 @@
 import { all } from "redux-saga/effects";
 import blogSaga from "./blog/saga";
 import authSaga from "./auth/saga";
+import settingsSaga from "./settings/saga";
 
 export default function* rootSaga() {
   yield all([
     blogSaga(),
     authSaga(),
+    settingsSaga(),
   ]);
 }
