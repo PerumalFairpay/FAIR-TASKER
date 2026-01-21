2601e5f4cc2baf08c5802110f59ac0302620f5ca
PerumalFairpay <perumal4fairpay@gmail.com>
2026-01-21 12:55:53 +0530

    Update AddEditEmployeeDrawer.tsx

--- app/employee/list/AddEditEmployeeDrawer.tsx
+++ app/employee/list/AddEditEmployeeDrawer.tsx
@@ -137,7 +137,7 @@ export default function AddEditEmployeeDrawer({
             setFormData({ ...fetchedEmployee });
         } else if (isOpen && mode === "create") {
             if (Object.keys(formData).length === 0) {
-                setFormData({ status: "Onboarding", work_mode: "Office" });
+                setFormData({ status: "Onboarding" });
             }
         }
     }, [fetchedEmployee, isOpen, mode, departments, rootDepartments]);
