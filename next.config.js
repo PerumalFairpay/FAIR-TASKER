6a6f132bb566d086c6687a7617dadf2d2cf40265
PerumalFairpay <perumal4fairpay@gmail.com>
2026-01-23 18:14:31 +0530

    save

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  reactStrictMode: false, // This will prevent double API calls in development
+};
 module.exports = nextConfig;
